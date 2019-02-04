ab2f5fee16d43ff957a0dcec79b9e73a3cac187b
Jean-François L'Heureux <spot_web@hotmail.com>
2019-02-04 14:52:39 -0500

    Add Feature.CoveoSearch module

---
+++ src/Feature/CoveoSearch/website/Controllers/SearchResourcesWithStylesheetController.cs
@@ -0,0 +1,18 @@
+﻿using Coveo.UI.Components.Sxa.Areas.CoveoHiveSxa.Controllers.Resources;
+using Coveo.UI.Components.Sxa.Areas.CoveoHiveSxa.Repositories.Resources;
+
+namespace Sitecore.HabitatHome.Feature.CoveoSearch.Controllers
+{
+    public class SearchResourcesWithStylesheetController : SearchResourcesController
+    {
+        public SearchResourcesWithStylesheetController(ISearchResourcesRepository p_Repository)
+            : base(p_Repository)
+        {
+        }
+
+        protected override string GetIndexViewName()
+        {
+            return "~/Views/Commerce/CoveoSearch/Coveo Search Resources With Stylesheet.cshtml";
+        }
+    }
+}
