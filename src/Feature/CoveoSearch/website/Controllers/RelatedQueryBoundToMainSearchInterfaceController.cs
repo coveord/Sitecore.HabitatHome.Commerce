e8b83291755b27a76f74ba6ed449b74888db3664
Jean-François L'Heureux <spot_web@hotmail.com>
2019-02-05 21:52:01 -0500

    Create and use custom related query component bound to the hardcoded main search interface

---
+++ src/Feature/CoveoSearch/website/Controllers/RelatedQueryBoundToMainSearchInterfaceController.cs
@@ -0,0 +1,18 @@
+﻿using Coveo.UI.Components.Sxa.Areas.CoveoHiveSxa.Controllers.SearchInterfaces;
+using Coveo.UI.Components.Sxa.Areas.CoveoHiveSxa.Repositories.SearchInterfaces;
+
+namespace Sitecore.HabitatHome.Feature.CoveoSearch.Controllers
+{
+    public class RelatedQueryBoundToMainSearchInterfaceController : RelatedQueryController
+    {
+        public RelatedQueryBoundToMainSearchInterfaceController(IRelatedQueryRepository p_Repository)
+            : base(p_Repository)
+        {
+        }
+
+        protected override string GetIndexViewName()
+        {
+            return "~/Views/Commerce/CoveoSearch/Coveo Related Query Bound to Main Search Interface.cshtml";
+        }
+    }
+}
