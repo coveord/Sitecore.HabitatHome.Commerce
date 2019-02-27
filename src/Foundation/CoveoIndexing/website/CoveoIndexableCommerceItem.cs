32e39dbbf3f9cd7c348ac3401eb6bb97d4353cd8
Jean-François L'Heureux <spot_web@hotmail.com>
2019-02-27 15:54:28 -0500

    Add ToString method to CoveoIndexableCommerceItem

--- src/Foundation/CoveoIndexing/website/CoveoIndexableCommerceItem.cs
+++ src/Foundation/CoveoIndexing/website/CoveoIndexableCommerceItem.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Sitecore.HabitatHome.Foundation.CoveoIndexing
 {
     public class CoveoIndexableCommerceItem : ICoveoIndexableCommerceItem
     {
         public IDictionary<string, object> Metadata { get; } = new Dictionary<string, object>();
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
     }
 }
